fd8473ce430777020d9be16227481091f3bb218c
rigomora77 <rmora@dilarce.com>
2023-03-28 19:23:04 -0600

    Se agrega carrito de compras y productos

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from products.models import Product
+
+from .models import Cart
+from .utils import get_or_create_cart
+
+def cart(request):  
+  cart = get_or_create_cart(request)
+
+  return render(request, 'carts/cart.html', {
+      
+  })
+
+def add(request):
+  cart = get_or_create_cart(request)
+  product = Product.objects.get(pk=request.POST.get('product_id'))
+
+  cart.products.add(product)
+
+  return render(request, 'carts/add.html', {
+    'product':product
+  })
